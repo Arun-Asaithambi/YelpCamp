371274e97e4ff4ce4f796825eca92fc5d7b951a1
Arun-Asaithambi <167624082+Arun-Asaithambi@users.noreply.github.com>
2024-04-30 21:43:17 +0530

    updated

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require ('../utils/catchAsync');
 const User = require('../models/user');
-const { reviewSchema } = require('../schemas');
 const passport = require ('passport');
-const localStrategy = require('passport-local');
 const bcrypt = require ('bcrypt');
-
+const localStrategy = require('passport-local');
 
 
 
@@ -17,11 +15,11 @@ router.get('/register', (req, res) =>{
 router.post('/register', catchAsync(async (req, res)=>{
     try {
         const {username, email, password} = req.body;
-        const hash = await bcrypt.hash(password, 12);
-        const user = new User({email, username, password:hash});
-        // const registeredUser = await User.register(user, password);
-        await user.save();
-        req.session.user_id = user._id;
+        // const hash = await bcrypt.hash(password, 12);
+        // const user = new User({email, username, password:hash});
+        const user = new User({username, password});
+        const registeredUser = await User.register(user, password);
+        // await user.save();
         req.flash('success','welcome to yelp camp!!');
         res.redirect('/campgrounds');
     } catch(e) {
@@ -30,14 +28,23 @@ router.post('/register', catchAsync(async (req, res)=>{
     }
 }));
 
+
 router.get('/login', (req, res)=>{
   res.render('users/login')
 });
 
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),(req, res) =>{
-  res.flash('success', 'Welcome back!!') 
+router.post('/login',async(req, res) =>{
+  const { username, password } = req.body;
+  const finduser = await User.findOne({ username, password });
+  if(finduser){
+  req.flash('success', 'Welcome back!!') 
   res.redirect('/campgrounds')
+  } 
+  else{
+    req.flash('error', 'somthing wrong in username or password');
+    res.redirect('login');
+  }
 });
 
 
