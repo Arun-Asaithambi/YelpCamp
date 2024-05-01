734051eef9c4de622116cfe75ca8c0fefa809173
Arun-Asaithambi <167624082+Arun-Asaithambi@users.noreply.github.com>
2024-05-01 10:19:45 +0530

    updated

--- routes/user.js
+++ routes/user.js
@@ -15,11 +15,8 @@ router.get('/register', (req, res) =>{
 router.post('/register', catchAsync(async (req, res)=>{
     try {
         const {username, email, password} = req.body;
-        // const hash = await bcrypt.hash(password, 12);
-        // const user = new User({email, username, password:hash});
         const user = new User({username, password});
         const registeredUser = await User.register(user, password);
-        // await user.save();
         req.flash('success','welcome to yelp camp!!');
         res.redirect('/campgrounds');
     } catch(e) {
@@ -34,18 +31,36 @@ router.get('/login', (req, res)=>{
 });
 
 
-router.post('/login',async(req, res) =>{
-  const { username, password } = req.body;
-  const finduser = await User.findOne({ username, password });
-  if(finduser){
-  req.flash('success', 'Welcome back!!') 
-  res.redirect('/campgrounds')
-  } 
-  else{
-    req.flash('error', 'somthing wrong in username or password');
-    res.redirect('login');
+passport.use(new localStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
   }
-});
+));
+
+
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) =>{
+  req.flash('success', 'welcome back');
+  res.redirect('/campgrounds');
+})
+
+// router.post('/login',async(req, res) =>{
+//   const { username, password } = req.body;
+//   const finduser = await User.findOne({ username, password });
+//   if(finduser){
+//   req.flash('success', 'Welcome back!!') 
+//   res.redirect('/campgrounds')
+//   } 
+//   else{
+//     req.flash('error', 'somthing wrong in username or password');
+//     res.redirect('login');
+//   }
+// });
 
 
 module.exports = router;
